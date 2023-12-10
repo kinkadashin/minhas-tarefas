c62891b4e1d2ff75ea0f4d8a3479ffda35e8da15
Lucas Tavares <lucastavaresffr@gmail.com>
2023-12-10 15:26:09 -0300

    adicionando Enums

---
+++ src/utils/enums/Tarefa.ts
@@ -0,0 +1,10 @@
+export enum Prioridade {
+  URGENT = 'urgente',
+  IMPORTANTE = 'importante',
+  NORMAL = 'normal'
+}
+
+export enum Status {
+  PENDENTE = 'pendente',
+  CONCLUIDA = 'concluída'
+}
