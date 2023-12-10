2676e67f1b1e6d3db6623b54af3e78ff22258560
Lucas Tavares <lucastavaresffr@gmail.com>
2023-12-10 14:48:10 -0300

    componentização adicionando atributos e state para trocar dos botoes

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Baixar fatura no Gmail',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer treino B',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            titulo={t.titulo}
+            descricao={t.descricao}
+            prioridade={t.prioridade}
+            status={t.status}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
