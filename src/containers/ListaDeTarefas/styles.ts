37c15c91cff00298affd5876319e0def30022728
Lucas Tavares <lucastavaresffr@gmail.com>
2023-12-10 18:24:07 -0300

    exibição de resultados de filtragem

--- src/containers/ListaDeTarefas/styles.ts
+++ src/containers/ListaDeTarefas/styles.ts
@@ -5,3 +5,9 @@ export const Container = styled.main`
   height: 100vh;
   overflow-y: scroll;
 `
+export const Resultado = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 40px;
+  margin-bottom: 40px;
+`
